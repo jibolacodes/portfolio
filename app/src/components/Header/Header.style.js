ed6fe297e09ff83f2ed494a8cceefeaad5a505c0
jibolacodes <jibzsupreme@gmail.com>
2022-07-20 09:18:31 +0100

    created header

--- app/src/components/Header/Header.style.js
+++ app/src/components/Header/Header.style.js
@@ -7,42 +7,69 @@ export const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;    
 `;
 
 export const Content = styled.div`
   text-align: center;
+  
+  @media only screen and (min-width: 1023px) {
+    text-align: start;
+    width: 970px;
+    display: grid;
+    align-items: center;
+    grid-template-columns: auto auto;
+  }
+`;
+
+export const UserImg = styled.img`
+  margin-bottom: 20px;
+  width: 300px;
+  clip-path: circle();
+
+  @media only screen and (min-width: 1023px) {
+    width: 300px;
+  }
+`;
+
+export const Info = styled.div`
+  
 `;
 
 export const Text = styled.div`
+  margin-bottom: 40px;
   h1 {
     font-weight: 600;
-    font-size: 2.6em;
+    font-size: 2.2em;
     color: #FF9D19;
     text-transform: uppercase;
     letter-spacing: 3px;
     font-family: 'Ubuntu', sans-serif;
+    margin: 0 auto;
+    text-shadow: 4px 4px #db2777;
+
+    @media only screen and (min-width: 1023px) {
+      font-size: 3rem;
+    }
   }
 
   h3 {
+    margin-bottom: 10px;
     font-weight: 400;
     color: rgb(211, 206, 198);
   }
 `;
 
-export const UserImg = styled.img`
-  width: 220px;
-  border-radius: 50%;
-`;
 
 export const AnimatedText = styled.div`
   max-width: fit-content;
   font-weight: 400;
-  font-size: 1.5em;
+  font-size: 1.3em;
   color: rgb(211, 206, 198);
   overflow: hidden;
   border-right: 0.15em solid #FF9D19;
   white-space: nowrap;
-  margin: 0 auto;
+  margin: 50px auto;
   letter-spacing: 0.12em;
   animation: 
     typing 3.5s steps(40, end),
@@ -57,14 +84,19 @@ export const AnimatedText = styled.div`
     from, to { border-color: transparent }
     50% { border-color: #FF9D19 }
   } 
+
+  @media only screen and (min-width: 1023px) {
+    margin: 0;
+    font-size: 1.7em;
+  }
 `;
 
 export const DownloadBtn = styled.div`
-  border: 1px solid #FF9D19;
+  border: 2px dotted #FF9D19;
   border-radius: 20px;
   width: fit-content;
   padding: 8px 20px;
-  margin: 2rem auto;
+  margin: 1rem auto;
 
   a {
     text-decoration: none;
@@ -80,5 +112,27 @@ export const DownloadBtn = styled.div`
       color: #272422;
       font-weight: 700;
     }
+  }
+  @media only screen and (min-width: 1023px) {
+    margin: 4rem 0;
+  }
+`;
+
+
+export const Arrow = styled.img`
+  width: 40px;
+  position: relative;
+  bottom: -80px;
+  animation: downward 1s infinite alternate;
+
+  @keyframes downward {
+    0%{ transform: translateY(0); }
+    100% { transform: translateY(-10px); }
+  }
+
+  @media only screen and (min-width: 1023px) {
+    position: absolute;
+    bottom: 0px;
+    width: 60px;
   }
 `;
--- app/src/components/Header/index.js
+++ app/src/components/Header/index.js
@@ -1,22 +1,41 @@
 import React from 'react';
 // Markup
-import { Wrapper, Content, UserImg, Text, AnimatedText, DownloadBtn } from './Header.style'
+import { Wrapper, Content, UserImg, Info, Text, AnimatedText, DownloadBtn, Arrow } from './Header.style'
 // Images
 import HeaderImg from '../../assets/Images/userImages/jibolacodes.jpg';
+import ArrowDown from '../../assets/Images/icons/angle-double-down.svg'
+import DownloadIcon from '../../assets/Images/icons/import.svg'
 
 const Header = () => (
   <Wrapper>
     <Content>
       <UserImg src={HeaderImg} alt="Jibolacodes image" />
-      <Text>
-        <h3>I'm</h3>
-        <h1>Ajibola Akindunbi</h1>
-      </Text>
-      <AnimatedText>Front-End Web Developer</AnimatedText>
-      <DownloadBtn>
-        <a href='#'>Download</a>
-      </DownloadBtn>
+
+      <Info>
+        <Text>
+          <h3>I'm</h3>
+          <h1>Ajibola Akindunbi</h1>
+        </Text>
+        <AnimatedText>Front-End Web Developer</AnimatedText>
+        <DownloadBtn>
+          <a href='#'>Download CV</a>
+          <img 
+            style={
+              {
+                'width': '20px',
+                'marginLeft': '15px',
+                'position': 'relative',
+                'top':'2px'
+              }
+            } 
+            src={DownloadIcon} 
+            alt="download icon" 
+            />
+        </DownloadBtn>
+      </Info>
+      
     </Content>
+    <Arrow src={ArrowDown} alt='Arrow Down' />
   </Wrapper>
 )
 
